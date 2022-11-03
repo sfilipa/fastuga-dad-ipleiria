c56e193120e0fb5536ccdda63ffd17b0bd45f86b
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2022-11-03 22:17:56 +0000

    Added Materio Template to Vue

--- fastuga-vue/src/main.js
+++ fastuga-vue/src/main.js
@@ -1,6 +1,16 @@
+/* eslint-disable import/order */
+import '@/@iconify/icons-bundle'
+import App from '@/App.vue'
+import vuetify from '@/plugins/vuetify'
+import { loadFonts } from '@/plugins/webfontloader'
+import router from '@/router'
+import '@/styles/styles.scss'
+import '@core/scss/index.scss'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import App from './App.vue'
-
-import './assets/main.css'
-
-createApp(App).mount('#app')
+loadFonts()
+const app = createApp(App)
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
