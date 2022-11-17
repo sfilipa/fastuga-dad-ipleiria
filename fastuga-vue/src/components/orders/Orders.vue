efde20e591718816651a331fa5f84bff1cf56001
André Pinto <andresantospinto26@gmail.com>
2022-11-17 20:22:07 +0000

    Add some filters for orders

--- fastuga-vue/src/components/orders/Orders.vue
+++ fastuga-vue/src/components/orders/Orders.vue
@@ -1,41 +1,60 @@
 <script setup>
-  import { ref, computed, onMounted, inject } from 'vue'
+  import { ref, computed, onMounted, inject, watch } from 'vue'
   import { useRouter } from 'vue-router'
   import OrdersTable from "./OrdersTable.vue"
 
   const axios = inject('axios')
   const router = useRouter()
 
+  const orders = ref([])
+  const costumer_id = ref(undefined)
+  const ticketNumber = ref(undefined)
+  const filterByType = ref('A')
+
   const LoadOrders = () => {
     axios.get(`/orders`)
       .then((response) => {
-        response.data.data.forEach(element => {
-          orders.value.push(element)
-        });
-        // orders.value = response.data.data
+         orders.value = response.data
       })
       .catch((error) => {
         console.log(error)
       })
   }
 
-  const createOrder = () => {
+  // watch(filterByType, (newValue, oldValue) => {
+  //   orders.value = []
+  //   axios.get(`/orders/status`, {
+  //     params: 'C'
+  //   })
+  //     .then((response) => {
+  //        orders.value = response.data
+  //     })
+  //     .catch((error) => {
+  //       console.log(error)
+  //     })
+  // })
 
-  }
+  // const createOrder = () => {
 
-  const searchByTicketNumber = () => {
+  // }
 
+  const showOrder = (order) => {
+    console.log("Show")  
+    const orderObj = Object.assign({}, order)
+    console.log(orderObj)
   }
 
   const editOrder = (order) => {
-
+    console.log("Edit")
+    const orderObj = Object.assign({}, order)
+    console.log(orderObj)
   }
 
   const deleteOrder = (order) => {
-
+    console.log("Delete")
+    const orderObj = Object.assign({}, order)
+    console.log(orderObj)
   }
-
-  const orders = ref([])
 
   onMounted (() => {
     LoadOrders()
@@ -49,58 +68,59 @@
     </div>
   </div>
   <hr>
-  <div
-    class="mb-3 d-flex justify-content-between flex-wrap"
-  >
+  <div class="mb-3 d-flex justify-content-between flex-wrap">
     <div class="mx-2 mt-2 flex-grow-1 filter-div">
-      <label
-        for="selectType"
-        class="form-label"
-      >Filter by Status:</label>
+      <label for="selectType" class="form-label">Filter by Status:</label>
       <select
         class="form-select"
         id="selectType"
         v-model="filterByType"
       >
-        <option value="-1">Any</option>
-        <option value="0">Preparing</option>
-        <option value="1">Ready</option>
-        <option value="2">Delivered</option>
-        <option value="3">Cancelled</option>
+        <option value="A">Any</option>
+        <option value="P">Preparing</option>
+        <option value="R">Ready</option>
+        <option value="D">Delivered</option>
+        <option value="C">Cancelled</option>
       </select>
     </div>
 
     <div class="mx-2 mt-2 flex-grow-1 filter-div">
-      <label
-        for="selectType"
-        class="form-label"
-      >Search for Ticket Number:</label>
+      <div class="inner-addon left-addon">
+        <label for="selectType" class="form-label">Search for Ticket Number:</label>
+        <i class="glyphicon glyphicon-user"></i>
+        <input v-model="ticketNumber" type="number" name="ticketnumber" class="form-control"/>
+        <!-- <form id="ticketNumberFilter"> -->
+        <!-- <input name="ticketnumber" v-model="ticketNumber"> -->
+        <!-- <button @click="searchByTicketNumber">Search</button> -->
+      <!-- </form> -->
+    </div>
+    </div>
 
-      <form id="tickerNumberFilter">
-        <input name="ticketnumber" v-model="ticketNumber">
-        <button @click="searchByTicketNumber">Search</button>
+    <div class="mx-2 mt-2 flex-grow-1 filter-div">
+      <label for="selectType" class="form-label">Search for Costumer:</label>
+      <form id="costumerFilter">
+        <input v-model="costumer_id" type="number" name="costumer_id" class="form-control">
+        <!-- <button @click="searchByCostumerId">Search</button> -->
       </form>
     </div>
 
-    <div class="mx-2 mt-2">
+    <!-- <div class="mx-2 mt-2">
       <button
         type="button"
         class="btn btn-success px-4"
         @click="createOrder"
       ><i class="bi bi-xs bi-plus-circle"></i>&nbsp; Create Order</button>
-    </div>
+    </div> -->
+
   </div> 
   <orders-table
     :orders="orders"
+    :filterByType="filterByType"
+    :ticketNumber="ticketNumber"
+    :costumerId="costumer_id"
+    @show="showOrder"
     @edit="editOrder"
     @delete="deleteOrder">
   </orders-table>
-
-  <order-item
-    v-for="item in orders"
-    :key="item.id"
-    :item="item"
-    >
-    </order-item>
 </template>
 
