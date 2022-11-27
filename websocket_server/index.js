c4ef207ccac114950df02ae31f052aa8a38fd780
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2022-11-27 11:43:43 +0000

    Installed Required Packages for WS and Connected it

--- websocket_server/index.js
+++ websocket_server/index.js
@@ -1,26 +1,15 @@
 const httpServer = require('http').createServer()
 const io = require("socket.io")(httpServer, {
-    cors: {
-        // The origin is the same as the Vue app domain:
-        // Change if necessary
-        origin: "http://127.0.0.1:5174",
-        methods: ["GET", "POST"],
-        credentials: true
-    }
+ cors: {
+ // The origin is the same as the Vue app domain. Change if necessary
+ origin: "http://127.0.0.1:5174",
+ methods: ["GET", "POST"],
+ credentials: true
+ }
 })
-httpServer.listen(8081, () =>{
-    console.log('listening on *:8081')
+httpServer.listen(8080, () =>{
+ console.log('listening on *:8080')
 })
 io.on('connection', (socket) => {
-    console.log(`client ${socket.id} has connected`)
-    socket.on('loggedIn', function (user) {
-        socket.join(user.id)
-        if (user.type == 'EM') {
-            socket.join('employeeManager')
-        }
-    })
-    socket.on('loggedOut', function (user) {
-        socket.leave(user.id)
-        socket.leave('employeeManager')
-    })
+ console.log(`client ${socket.id} has connected`)
 })
