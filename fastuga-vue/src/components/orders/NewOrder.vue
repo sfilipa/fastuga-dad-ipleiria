33dc821b9331af1e334ac65ed044fb9a0eb08aea
André Pinto <andresantospinto26@gmail.com>
2022-11-30 17:55:41 +0000

    Add payment confirm logic and validations

--- fastuga-vue/src/components/orders/NewOrder.vue
+++ fastuga-vue/src/components/orders/NewOrder.vue
@@ -1,22 +1,100 @@
 <script setup>
-  import { ref, computed, onMounted, inject, watch } from 'vue'
-  import { useRouter } from 'vue-router'
-  import { useOrderItemsStore } from '@/stores/orderItems.js'
-  import axios from 'axios'
-  
-  const store = useOrderItemsStore()
-  const paymentType = ref("visa")
+    import { ref, computed, onMounted, inject, watch } from 'vue'
+    import { useRouter } from 'vue-router'
+    import { useOrderItemsStore } from '@/stores/orderItems.js'
+    import axios from 'axios'
 
-  const deleteClick = (product) => {
-    const idx = store.items.findIndex((element) => element === product)
-    if (idx >= 0) {
-        store.items.splice(idx, 1)
+    const PAYMENT_URL = 'https://dad-202223-payments-api.vercel.app' 
+    const store = useOrderItemsStore()
+    const paymentType = ref("visa")
+    const paymentReference = ref("")
+    //   const axios = inject("axios")
+    //   const inputDiv = null
+    const referenceFocus = ref(null)
+    const errors = ref(null)
+
+    const deleteClick = (product) => {
+        const idx = store.items.findIndex((element) => element === product)
+        if (idx >= 0) {
+            store.items.splice(idx, 1)
+        }
     }
-  }
 
-  const confirmPayment = () => {
+    const refocus = () => {
+        paymentReference.value = ""
+        referenceFocus.value.focus()
+    }
 
-  }
+    const confirmPayment = () => {
+
+        if(paymentReference.value.length == 0){
+            errors.value = {
+            default: ["Empty Reference Value"]
+            }
+            return 
+        }
+        if(paymentType.value == 'visa'){
+            if(paymentReference.value.match('[1-9][0-9]{15}')){
+                console.log("matches") 
+            }else{
+                errors.value = {
+                visa: ["Invalid Visa Reference"]
+                }
+                return
+            }
+        }else if(paymentType.value == 'mbway'){
+            if(paymentReference.value.match('[1-9][0-9]{8}')){
+                console.log("matches") 
+            }else{
+                errors.value = {
+                mbway: ["Invalid Phone Number"]
+                }
+                return
+            }
+        }else if(paymentType.value == 'paypal'){
+            if(paymentReference.value.match('[a-zA-Z0-9.+_]+@[a-zA-Z0-9.+_]+.[a-zA-Z]')){
+                console.log("matches") 
+            }else{
+                errors.value = {
+                paypal: ["Invalid Email Format"]
+                }
+                return
+            }
+        }else{
+            errors.value = {
+                default: ["Payment Type Not Supported"]
+            }
+            return
+        }
+
+        if(store.totalPrice <= 0){
+            errors.value = {
+                default: ["Price Must Be Higher Than Zero!"]
+            }
+            return
+        }
+
+        const requestBody = {
+                'type': paymentType.value,
+                'reference': paymentReference.value,
+                'value': store.totalPrice
+        }
+        
+        axios.post(`${PAYMENT_URL}/api/payments`, requestBody)
+            .then((response) => {
+                //if successful, create order in DB with correct status (check if has a hot dish or not)
+                console.log(response)
+            })
+            .catch((error) => {
+                if (error.response.status == 422) {
+                toast.error('Order was not created due to validation errors!')
+                errors.value = error.response.data.errors
+                } else {
+                toast.error('Order was not created due to unknown server error!')
+                errors.value = error.response.data.errors
+                }
+            })
+    }
 
 </script>
 
@@ -30,7 +108,7 @@
         <table class="table">
             <thead>
             <tr>
-                <!-- <th>Image</th> -->
+                <th>Image</th>
                 <th>Name</th>
                 <th>Type</th>
                 <th>Price</th>
@@ -40,9 +118,9 @@
             <tbody>
             <tr v-for="product in store.items" 
                 :key="product.id">
-                <!-- <td>
-                    <img :src='"http://localhost:8081/storage/products/"+product.photo_url' />
-                </td> -->
+                <td>
+                    <img :src='"http://localhost:8081/storage/products/" + product.photo_url' />
+                </td>
                 <td>
                     {{ product.name }}
                 </td>
@@ -69,41 +147,67 @@
         <h2 class="text-center">Select Payment Type:</h2>
         <div class="paymentChoice">
             <ul>
-                <li><input type="radio" name="payment" v-model="paymentType" value="visa" id="visa">Visa</li>
-                <li><input type="radio" name="payment" v-model="paymentType" value="mbway" id="mbway">MbWay</li>
-                <li><input type="radio" name="payment" v-model="paymentType" value="paypal" id="paypal">PayPal</li>
+                <li><input @change="refocus" type="radio" name="payment" v-model="paymentType" value="visa" id="visa">Visa</li>
+                <li><input @change="refocus" type="radio" name="payment" v-model="paymentType" value="mbway" id="mbway">MbWay</li>
+                <li><input @change="refocus" type="radio" name="payment" v-model="paymentType" value="paypal" id="paypal">PayPal</li>
             </ul>
-            <!-- <div>
-                <input type="radio" name="payment" v-model="paymentType" value="visa" id="visa">
-                <label class="radio-inline" for="visa">Visa</label>
-            </div>
-            <div>
-                <input type="radio" name="payment" v-model="paymentType" value="mbway" id="mbway">
-                <label class="radio-inline" for="mbway">MbWay</label>
-            </div>
-            <div>
-                <input type="radio" name="payment" v-model="paymentType" value="paypal" id="paypal">
-                <label class="radio-inline" for="paypal">PayPal</label>
-            </div> -->
         </div>
         <div v-if="paymentType=='visa'">
             <div>
                 <label>Visa Card ID:</label>
-                <input class="form-control" type="text" />
+                <input ref="referenceFocus" class="form-control" type="text" v-model="paymentReference"/>
+                <field-error-message :errors="errors" fieldName="visa"></field-error-message>
             </div>
         </div>
         <div v-else-if="paymentType=='mbway'">
             <div>
                 <label>Phone Number:</label>
-                <input class="form-control" type="text" />
+                <input ref="referenceFocus" class="form-control" type="text" v-model="paymentReference"/>
+                <field-error-message :errors="errors" fieldName="mbway"></field-error-message>
             </div>
         </div>
         <div v-else>
             <div>
                 <label>Email:</label>
-                <input class="form-control" type="text" />
+                <input ref="referenceFocus" class="form-control" type="text" v-model="paymentReference"/>
+                <field-error-message :errors="errors" fieldName="paypal"></field-error-message>
             </div>
         </div>
+        <field-error-message :errors="errors" fieldName="default"></field-error-message>
+        <!-- <div class="payment-box text-center"> 
+            <div >
+                <input ref="referenceFocus" @change="refocus" type="radio" name="payment" v-model="paymentType" value="visa" id="visa">
+                <label for="visa">Visa</label>
+            
+                <input ref="referenceFocus" @change="refocus" type="radio" name="payment" v-model="paymentType" value="mbway" id="mbway">
+                <label for="mbway">MBWay</label>
+            
+                <input ref="referenceFocus" @change="refocus" type="radio" name="payment" v-model="paymentType" value="paypal" id="paypal">
+                <label for="paypal">PayPal</label>
+            </div>
+            
+             <div class="btn-group payment-choice">
+                <input ref="referenceFocus" @change="refocus" type="radio" class="btn-check" name="payment" v-model="paymentType" value="visa" id="visa">
+                <label class="btn btn-secondary" for="visa">Visa</label>
+            
+                <input ref="referenceFocus" @change="refocus" type="radio" class="btn-check" name="payment" v-model="paymentType" value="mbway" id="mbway">
+                <label class="btn btn-secondary" for="mbway">MBWay</label>
+            
+                <input ref="referenceFocus" @change="refocus" type="radio" class="btn-check" name="payment" v-model="paymentType" value="paypal" id="paypal">
+                <label class="btn btn-secondary" for="paypal">PayPal</label>
+            </div> 
+            
+                <div v-if="paymentType=='visa'">
+                    <input v-model="paymentReference" class="input-group input-group-sm" type="text" placeholder="Insert Visa Card ID"/>
+                </div>
+                <div v-else-if="paymentType=='mbway'">
+                    <input v-model="paymentReference" class="input-group input-group-sm" type="text" placeholder="Insert Phone Number"/>
+                </div>
+                <div v-else>
+                    <input v-model="paymentReference" class="input-group input-group-sm" type="text" placeholder="Insert Email Address"/>
+                </div>
+            
+        </div> -->
         <br>
         <button
             type="button"
@@ -114,21 +218,22 @@
 </template>
 
 <style>
-.paymentChoice {
-  width: 100%;
-  font-size: 125%
+.payment-box{
+    margin: auto;
+    height: 110px;
+    width: 35%;
+    position: relative;
+    border: 1px solid rgb(12, 12, 12);
 }
-.paymentChoice ul{
-  width: 100%;
-  text-align: center;
-}
-.paymentChoice ul li{
-  margin: 10px 10px 0px 0px;
-  display: inline-block;
-}
+
 input[type="radio"]{
-  margin-right: 10px;
-  cursor: pointer;
+    margin-right: 10px;
+    cursor: pointer;
+}
+
+img, svg {
+    vertical-align: middle;
+    width: 65px;
 }
 
 </style>
